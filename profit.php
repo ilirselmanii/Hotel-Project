2ea870150ad3ddb7a80d4a96b99cfb2d4569dca8
ardian-sa <ardiansallauka1@gmail.com>
2023-02-13 00:03:58 +0100

    profit4

--- profit.php
+++ profit.php
@@ -207,3 +207,49 @@ if(!isset($_SESSION["user"]))
                         </div>
                     </div>
                     <!--End Advanced Tables -->
+                    </div>
+            </div>
+                <!-- /. ROW  -->
+
+                </div>
+
+            </div>
+
+
+    </div>
+             <!-- /. PAGE INNER  -->
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+     <!-- /. WRAPPER  -->
+    <!-- JS Scripts-->
+    <!-- jQuery Js -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+      <!-- Bootstrap Js -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- Metis Menu Js -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+     <!-- DATA TABLE SCRIPTS -->
+    <script src="assets/js/dataTables/jquery.dataTables.js"></script>
+    <script src="assets/js/dataTables/dataTables.bootstrap.js"></script>
+        <script>
+            $(document).ready(function () {
+                $('#dataTables-example').dataTable();
+            });
+    </script>
+         <!-- Custom Js -->
+    <script src="assets/js/custom-scripts.js"></script>
+
+
+</body>
+</html>
+<script>
+Morris.Bar({
+ element : 'chart',
+ data:[<?php echo $chart_data; ?>],
+ xkey:'date',
+ ykeys:['profit'],
+ labels:['Profit'],
+ hideHover:'auto',
+ stacked:true
+});
+</script>
