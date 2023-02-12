9a151258350e8a0a9defc06b38fddc6cd8b3595c
ilirselmanii <selmani.alb123@gmail.com>
2023-02-12 23:27:15 +0100

    messages3

--- messages.php
+++ messages.php
@@ -171,3 +171,70 @@ if(!isset($_SESSION["user"]))
 								}
 								
 							}
+                            ?>
+                          
+                        </p>
+						
+                    </div>
+                </div>
+            </div>
+               <?php
+				
+				$sql = "SELECT * FROM `contact`";
+				$re = mysqli_query($con,$sql);
+				
+			   ?>
+            <div class="row">
+                <div class="col-md-12">
+                    <!-- Advanced Tables -->
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                    <thead>
+                                        <tr>
+                                            <th>Name</th>
+											<th>Phone Number</th>
+                                            <th>Email</th>
+                                            <th>Date</th>
+											<th>Status</th>
+											<th>Approval</th>
+											<th>Remove</th>
+                                            
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        
+									<?php
+										while($row = mysqli_fetch_array($re))
+										{
+										
+											$id = $row['id'];
+											
+											if($id % 2 ==1 )
+											{
+												echo"<tr class='gradeC'>
+													<td>".$row['fullname']."</td>
+													<td>".$row['phoneno']."</td>
+													<td>".$row['email']."</td>
+													<td>".$row['cdate']."</td>
+													<td>".$row['approval']."</td>
+													<td><a href=newsletter.php?eid=".$id ." <button class='btn btn-primary'> <i class='fa fa-edit' ></i> Permission</button></td>
+													<td><a href=newsletterdel.php?eid=".$id ." <button class='btn btn-danger'> <i class='fa fa-edit' ></i> Delete</button></td>
+												</tr>";
+											}
+											else
+											{
+												echo"<tr class='gradeU'>
+													<td>".$row['fullname']."</td>
+													<td>".$row['phoneno']."</td>
+													<td>".$row['email']."</td>
+													<td>".$row['cdate']."</td>
+													<td>".$row['approval']."</td>
+													<td><a href=newsletter.php?eid=".$id." <button class='btn btn-primary'> <i class='fa fa-edit' ></i> Permission</button></td>
+													<td><a href=newsletterdel.php?eid=".$id ." <button class='btn btn-danger'> <i class='fa fa-edit' ></i> Delete </button></td>		
+												</tr>";
+											
+											}
+										
+										}
