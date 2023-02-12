4dc106d4e6b5e5ac9a4b73739905c5aeddedd913
ardian-sa <ardiansallauka1@gmail.com>
2023-02-12 23:36:34 +0100

    newsletterdel2

--- newsletterdel.php
+++ newsletterdel.php
@@ -13,3 +13,19 @@ echo '<script>alert("Sorry ! Wrong Entry") </script>' ;
 
 else{
 $view="DELETE FROM `contact` WHERE id ='$id' ";
+if($re = mysqli_query($con,$view))
+    {
+        echo '<script>alert("News Letter Subscriber Remove") </script>' ;
+        header("Location: messages.php");
+    }
+
+
+}
+
+
+
+
+
+
+
+?>
