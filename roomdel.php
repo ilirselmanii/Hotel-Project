174fe9aa4bc2b2a8cedd0485b9740b766117097a
ilirselmanii <selmani.alb123@gmail.com>
2023-02-13 00:25:51 +0100

    roomdel3

--- roomdel.php
+++ roomdel.php
@@ -157,4 +157,100 @@ $rre=mysqli_query($con,$rsql);
 				?>
                 <div class="row">
                  
-                                 
+                <?php
+										while($row= mysqli_fetch_array($re))
+										{
+												$id = $row['type'];
+											if($id == "Superior Room") 
+											{
+												echo"<div class='col-md-3 col-sm-12 col-xs-12'>
+													<div class='panel panel-primary text-center no-boder bg-color-blue'>
+														<div class='panel-body'>
+															<i class='fa fa-users fa-5x'></i>
+															<h3>".$row['bedding']."</h3>
+														</div>
+														<div class='panel-footer back-footer-blue'>
+															".$row['type']."
+
+														</div>
+													</div>
+												</div>";
+											}
+											else if ($id == "Deluxe Room")
+											{
+												echo"<div class='col-md-3 col-sm-12 col-xs-12'>
+													<div class='panel panel-primary text-center no-boder bg-color-green'>
+														<div class='panel-body'>
+															<i class='fa fa-users fa-5x'></i>
+															<h3>".$row['bedding']."</h3>
+														</div>
+														<div class='panel-footer back-footer-green'>
+															".$row['type']."
+
+														</div>
+													</div>
+												</div>";
+											
+											}
+											else if($id =="Guest House")
+											{
+												echo"<div class='col-md-3 col-sm-12 col-xs-12'>
+													<div class='panel panel-primary text-center no-boder bg-color-brown'>
+														<div class='panel-body'>
+															<i class='fa fa-users fa-5x'></i>
+															<h3>".$row['bedding']."</h3>
+														</div>
+														<div class='panel-footer back-footer-brown'>
+															".$row['type']."
+
+														</div>
+													</div>
+												</div>";
+											
+											}
+											else if($id =="Single Room")
+											{
+												echo"<div class='col-md-3 col-sm-12 col-xs-12'>
+													<div class='panel panel-primary text-center no-boder bg-color-red'>
+														<div class='panel-body'>
+															<i class='fa fa-users fa-5x'></i>
+															<h3>".$row['bedding']."</h3>
+														</div>
+														<div class='panel-footer back-footer-red'>
+															".$row['type']."
+
+														</div>
+													</div>
+												</div>";
+											
+											}
+										}
+									?>
+                    
+                </div>
+            <?php
+				
+			ob_end_flush();
+			?>
+                    
+            
+				
+					</div>
+			 <!-- /. PAGE INNER  -->
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+        </div>
+     <!-- /. WRAPPER  -->
+    <!-- JS Scripts-->
+    <!-- jQuery Js -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+      <!-- Bootstrap Js -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- Metis Menu Js -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+      <!-- Custom Js -->
+    <script src="assets/js/custom-scripts.js"></script>
+    
+   
+</body>
+</html>
