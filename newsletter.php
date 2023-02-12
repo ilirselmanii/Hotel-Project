7759cb0b177846b5bd45b0f8f29c00501df61eb3
ardian-sa <ardiansallauka1@gmail.com>
2023-02-12 23:33:55 +0100

    newsletter2

--- newsletter.php
+++ newsletter.php
@@ -12,3 +12,24 @@ while ($row=mysqli_fetch_array($re))
 	$id =$row['approval'];
 
 }
+if($id=="Not Allowed")
+{
+    $sql ="UPDATE contact SET approval= '$approval' WHERE id = '$eid' ";
+    if(mysqli_query($con,$sql))
+    {
+        echo '<script>alert("New Room Added") </script>' ;
+        header("Location: messages.php");
+    }
+}
+else {
+$sql ="UPDATE contact SET approval= '$napproval' WHERE id = '$eid' ";
+    if(mysqli_query($con,$sql))
+    {
+        echo '<script>alert("New Room Added") </script>' ;
+        header("Location: messages.php");
+    }
+
+
+
+}
+?>
