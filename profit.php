ce6dab75ce8a7cd9d7c8dbdd953d67fe1c00ad00
ardian-sa <ardiansallauka1@gmail.com>
2023-02-13 00:01:21 +0100

    profit2

--- profit.php
+++ profit.php
@@ -93,3 +93,64 @@ if(!isset($_SESSION["user"]))
             </div>
 
         </nav>
+        <div id="page-wrapper" >
+            <div id="page-inner">
+			 <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="page-header">
+                           Profit Details<small> </small>
+                        </h1>
+                    </div>
+                </div> 
+                 <!-- /. ROW  -->
+				 
+				 
+            <div class="row">
+			
+				<?php 
+				//index.php
+				//$connect = mysqli_connect("localhost", "root", "", "hotel");
+				include('db.php');
+				
+					
+					$query = "SELECT * FROM payment";
+					$result = mysqli_query($con, $query);
+					$chart_data = '';
+					$tot = 0;
+					while($row = mysqli_fetch_array($result))
+					{
+					 $chart_data .= "{ date:'".$row["cout"]."', profit:".$row["fintot"] *10/100 ."}, ";
+					 $tot = $tot + $row["fintot"] *10/100;
+					}
+					$chart_data = substr($chart_data, 0, -2);
+				
+?>
+				 
+				<br>
+				<br>
+				<br>
+				<br><div id="chart"></div>
+                <div class="col-md-12">
+                    <!-- Advanced Tables -->
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                    <thead>
+                                        <tr>
+											<th>ID</th>
+                                            <th>Name</th>
+                                            <th>Check in</th>
+											<th>Check out</th>
+                                            <th>Room Rent</th>
+											<th>Bed Rent</th>
+											<th>Meals </th>
+											<th>Gr.Total</th>
+											<th>Profit</th>
+											
+                                            
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        
+									<?php
